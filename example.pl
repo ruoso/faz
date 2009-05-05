67fbc0640e7c8f065a8cc019275a596f05da11d0
Daniel Ruoso <daniel@ruoso.com>
2009-05-05 15:47:17 -0300

    use some base classes for examples

--- example.pl
+++ example.pl
@@ -1,5 +1,7 @@
 module MyApp;
 
+use Faz::Request;
+use Faz::URI;
 use Faz::Application;
 use Faz::Controller;
 use Faz::Action::Root;
@@ -9,15 +11,17 @@ use Faz::Action::Public;
 my $app = Faz::Application.new(:dispatcher(Faz::Dispatcher.new()));
 my $ctrl = Faz::Controller.new();
 
-my $root = Faz::Action::Root.new( :controller($ctrl),
-                                  :private-name('(root)/'),
-                                  :base('http://localhost/'),
+my $root = Faz::Action::Root.new( :parent(0),
+                                  :controller($ctrl),
+                                  :private-name('(root)'),
+                                  :base('/'),
                                   :begin-closure({ say "root - begin" }),
                                   :execute-closure({ say "root - execute " }),
                                   :end-closure({ say "root - end" }) );
 $app.register-action($root);
 
-my $blog = Faz::Action::Chained.new( :controller($ctrl),
+my $blog = Faz::Action::Chained.new( :parent($root),
+                                     :controller($ctrl),
                                      :private-name('(root)/blog/*'),
                                      :regex(/ blog\/(\w+) /),
                                      :begin-closure({ say "blog - begin" }),
@@ -25,7 +29,8 @@ my $blog = Faz::Action::Chained.new( :controller($ctrl),
                                      :end-closure({ say "blog - end" }) );
 $app.register-action($blog);
 
-my $viewblog = Faz::Action::Public.new( :controller($ctrl),
+my $viewblog = Faz::Action::Public.new( :parent($blog),
+                                        :controller($ctrl),
                                         :private-name('(root)/blog/*/'),
                                         :regex(/^ $/),
                                         :begin-closure({ say "viewblog - begin" }),
@@ -33,3 +38,9 @@ my $viewblog = Faz::Action::Public.new( :controller($ctrl),
                                         :end-closure({ say "viewblog - end" }) );
 $app.register-action($viewblog);
 
+my $uri = Faz::URI.new(:path('/blog/faz'));
+my $request is context = Faz::Request.new(:uri($uri));
+my $response is context = 1;
+$app.handle($request,$response);
+
+1;
