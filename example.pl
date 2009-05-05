2f85c91bbdea772a29c66e6d604695fa9c796e23
Daniel Ruoso <daniel@ruoso.com>
2009-05-05 14:18:43 -0300

    define the controller into the actions

--- example.pl
+++ example.pl
@@ -3,24 +3,26 @@ module MyApp;
 use Faz::Application;
 
 my $app = Faz::Application.new();
-$app.dispatcher = Faz::Dispatcher.new();
 my $ctrl = Faz::Controller.new();
 
-my $root = Faz::Action::Root.new( :private-name('(root)/'),
+my $root = Faz::Action::Root.new( :controller($ctrl),
+                                  :private-name('(root)/'),
                                   :base('http://localhost/'),
                                   :begin-closure({ say "root - begin" }),
                                   :execute-closure({ say "root - execute " }),
                                   :end-closure({ say "root - end" }) );
 $app.register-action($root);
 
-my $blog = Faz::Action::Chained.new( :private-name('(root)/blog/*'),
+my $blog = Faz::Action::Chained.new( :controller($ctrl),
+                                     :private-name('(root)/blog/*'),
                                      :regex(/ blog\/(\w+) /),
                                      :begin-closure({ say "blog - begin" }),
                                      :execute-closure(-> $name { say "blog($name) - execute " }),
                                      :end-closure({ say "blog - end" }) );
 $app.register-action($blog);
 
-my $viewblog = Faz::Action::Public.new( :private-name('(root)/blog/*/'),
+my $viewblog = Faz::Action::Public.new( :controller($ctrl),
+                                        :private-name('(root)/blog/*/'),
                                         :regex(/^ $/),
                                         :begin-closure({ say "viewblog - begin" }),
                                         :execute-closure({ say "viewblog - execute" }),
